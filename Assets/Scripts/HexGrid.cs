fe8b562406786cfe51fe12695acfc1f3b6350125
Max <jcarvajal@crimson.ua.edu>
2018-12-02 18:26:51 -0600

    Hexes change color on mouseclick

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -10,6 +10,9 @@ public class HexGrid : MonoBehaviour
     public HexCell cellPrefab;
     public Text cellLabelPrefab;
 
+    public Color defaultColor = Color.white;
+    public Color touchedColor = Color.magenta;
+
     Canvas gridCanvas;
     HexCell[] cells;
     HexMesh hexMesh;
@@ -37,6 +40,10 @@ public class HexGrid : MonoBehaviour
         position = transform.InverseTransformPoint(position);
         HexCoordinates coordinates = HexCoordinates.FromPosition(position);
         Debug.Log("touched at " + position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        cell.color = touchedColor;
+        hexMesh.Triangulate(cells);
     }
 
     void Awake() {
@@ -64,6 +71,7 @@ public class HexGrid : MonoBehaviour
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
         cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defaultColor;
 
         Text label = Instantiate<Text>(cellLabelPrefab);
         label.rectTransform.SetParent(gridCanvas.transform, false);
